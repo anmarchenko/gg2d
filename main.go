ef0f1d9b407614edf75a71e87eb3453f7110a190
Andrey Marchenko <andrey.marchenko@share-now.com>
2022-01-27 16:27:02 +0100

    draw background image

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var (
+	err        error
+	background *ebiten.Image
+)
+
 // Game implements ebiten.Game interface.
 type Game struct{}
 
@@ -19,23 +24,30 @@ func (g *Game) Update(screen *ebiten.Image) error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	if ebiten.IsDrawingSkipped() {
-		return
-	}
 	// Write your game's rendering.
-	ebitenutil.DebugPrint(screen, "Hello World")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+
+	screen.DrawImage(background, op)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 160, 120
+	return 640, 480
+}
+
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	game := &Game{}
 
-	ebiten.SetWindowSize(320, 240)
+	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("GG2D")
 
 	if err := ebiten.RunGame(game); err != nil {
